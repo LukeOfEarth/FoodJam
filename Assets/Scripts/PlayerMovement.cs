4eaf6da6504925c22ddb0a2543146921cb219675
SJ-Jordaan <u18074848@tuks.co.za>
2021-09-06 08:47:00 +0200

    Add player movement, and layer handling

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerController playerController;
+    public float runSpeed = 40f;
+
+    private float horizontalMove = 0f;
+    private bool jump = false;
+    // Update is called once per frame
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        playerController.Move(Time.fixedDeltaTime * horizontalMove, jump);
+        jump = false;
+    }
+}
